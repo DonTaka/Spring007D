10ea9a8cddc8f573cbc31f642c348b204ed8ced6
j.riquelmee <j.riquelmee@profesor.duoc.cl>
2025-04-22 16:50:35 -0400

    Initial commit

---
+++ src/main/java/com/example/Spring007D/Spring007DApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring007D;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring007DApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring007DApplication.class, args);
+	}
+
+}
